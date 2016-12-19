32ba17112cf5e36fb7ca0e549b5b829b99f84cfb
Sebastian Ramacher <sramacher@debian.org>
2016-12-19 18:55:30 +0100

    Delete record iterator

--- DebianDevelChangesBot/datasources/aptarchive.py
+++ DebianDevelChangesBot/datasources/aptarchive.py
@@ -77,6 +77,9 @@ class AptArchive(NewDataSource):
                 if records.lookup(candidate.file_list[0]):
                     return split_address(records.maintainer)
 
+                # delate record iterator
+                del records
+
         if package.startswith('src:'):
             package = package[4:]
 
