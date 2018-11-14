b66b1df022181cb8b9cdb5056466dd125b3d8211
Sebastian Ramacher <sramacher@debian.org>
2018-11-14 21:28:14 +0100

    Apply black

--- setup.py
+++ setup.py
@@ -2,7 +2,14 @@
 
 from setuptools import setup, find_packages
 
-setup(name='debian-devel-changes-bot',
-      packages=find_packages(),
-      install_requires=['beautifulsoup4', 'python-debian', 'requests', 'pydbus',
-                        'python-apt'])
+setup(
+    name="debian-devel-changes-bot",
+    packages=find_packages(),
+    install_requires=[
+        "beautifulsoup4",
+        "python-debian",
+        "requests",
+        "pydbus",
+        "python-apt",
+    ],
+)
